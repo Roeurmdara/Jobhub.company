8e81dca8070e57ff4bff6f2a24fa9b7ff9560787
Rorurmdara <rouermdara9200@gmail.com>
2025-08-23 01:15:40 +0700

    dara

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo1 from"../images/logo1.png";
 function Navbar() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -24,12 +24,12 @@ function Navbar() {
 
     {/* Right Section */}
     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button
-        type="button"
-        className="text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 "
-      >
-        Get started
-      </button>
+     <button
+  type="button"
+  className="bg-blue-600 text-white hover:bg-blue-700 font-medium rounded-lg text-sm px-4 py-2"
+>
+  Get started
+</button>
     </div>
 
     {/* Menu Links */}
@@ -39,20 +39,20 @@ function Navbar() {
     >
       <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
         <li>
-          <Link
+          <NavLink
             to="/browse-companies"
             className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
           >
             Browse Companies
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <a
-            href="/company-dashboard"
+          <NavLink
+            to="/company-dashboard"
             className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
           >
             DashBoard
-          </a>
+          </NavLink>
         </li>
       </ul>
     </div>
